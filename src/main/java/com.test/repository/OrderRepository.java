c6430545bef4bc4af3c970beca75f1eea5e3b042
sergey <you@example.com>
2018-01-17 23:40:55 +0700

    add create

--- src/main/java/com.test/repository/OrderRepository.java
+++ src/main/java/com.test/repository/OrderRepository.java
@@ -1,13 +1,37 @@
 package com.test.repository;
 
+import com.test.commons.DatabaseUtils;
 import com.test.entity.Order;
 
-import java.sql.PreparedStatement;
+import java.sql.*;
 
 
 public class OrderRepository {
-    public Order create(Order order) {
-//        String sql = "INSERT INTO orders (full_name, telephone) VALUES (?, ?)";
+
+    private static final String INSERT_ORDER = "INSERT INTO orders (full_name, telephone) VALUES (?, ?)";
+    private static final String INSERT_PRODUCT = "UPDATE product SET order_id=? WHERE id=?";
+
+
+
+    public static Order create(Order order) {
+//
+        Connection connection = DatabaseUtils.getConnection();
+        try {
+            PreparedStatement statement = connection.prepareStatement(INSERT_ORDER,
+                    Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1,order.getFullName());
+            statement.setString(2,order.getTelephone());
+            statement.executeQuery();
+            order.setId(statement.getGeneratedKeys().getLong(1));
+            Statement statementProduct = connection.createStatement();
+//            order.getList().forEach(element ->{
+//
+//            });
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return order;
 //        KeyHolder keyHolder = new GeneratedKeyHolder();
 //        jdbcTemplate.update(connection -> {
 //            PreparedStatement ps =
@@ -27,6 +51,6 @@ public class OrderRepository {
 //                }
 //        );
 //
-        return null;
+//        return null;
     }
 }
